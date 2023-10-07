d720aeff4f95d7a626a85f66973d5f6d0e857370
Samuel Rivas <robertbbenn@gmail.com>
2023-10-07 03:35:48 -0500

    created indes.html and script.js files

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log('Camcho Rastafari')
+const body = document.querySelector('body')
+body.style.backgroundColor = '#111';
+body.style.color = '#fff';
+body.style.display = 'grid'
+body.style.height = '100vh'
+body.style.placeItems = "center"
+body.style.textAlign = "center"
+body.innerText = "EL HUEVO ENMASCARADO 🥚"
